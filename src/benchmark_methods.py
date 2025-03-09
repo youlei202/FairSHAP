c48c5fa193b2f736b6e03a56459915899c36504a
Linzhu <zhulin252@outlook.com>
2025-03-09 14:38:25 +0100

    add SOTA-reweighing method

--- src/benchmark_methods.py
+++ src/benchmark_methods.py
@@ -9,6 +9,9 @@ from fairlearn.preprocessing import CorrelationRemover
 from sklearn.model_selection import KFold
 import aif360
 from aif360.algorithms.preprocessing import DisparateImpactRemover
+from aif360.algorithms.preprocessing import Reweighing
+from aif360.algorithms.preprocessing import OptimPreproc
+
 from src.attribution import FairnessExplainer
 from src.data.unified_dataloader import load_dataset
 from src.attribution.oracle_metric import perturb_numpy_ver
@@ -19,7 +22,7 @@ import os
 class BenchMarkPreprocessingMethods:
     def __init__(self,
                 # dataset_name: str,  # 'german_Credit', 'compas', 'compas4race', 'adult', 'default_credit', 'census_income_kdd'
-                sota_method: str,  # 'disparate_impact', 'correlation_removal', 'reweight', 'fairUS', 'optimized_preprocessing'
+                sota_method: str,  # 'disparate_impact', 'correlation_removal', 'reweighing', 'fairUS', 'optimized_preprocessing'
     ):
         # self.dataset_name = dataset_name
         self.sota_method = sota_method
@@ -55,21 +58,27 @@ class BenchMarkPreprocessingMethods:
             if self.dataset_name == 'german_credit':
                 self.sen_att_name = 'sex'
                 self.target_name = 'Risk'
+                self.priv = 0
             elif self.dataset_name == 'adult':
                 self.sen_att_name = 'sex'
                 self.target_name = 'income'
+                self.priv = 1
             elif self.dataset_name == 'compas':
                 self.sen_att_name = 'sex'
                 self.target_name = 'two_year_recid'
+                self.priv =1
             elif self.dataset_name == 'compas4race':
                 self.sen_att_name = 'race'
                 self.target_name = 'two_year_recid'
+                self.priv = 1
             elif self.dataset_name == 'census_income_kdd':
                 self.sen_att_name = 'sex'
                 self.target_name = 'class'
+                self.priv = 0
             elif self.dataset_name == 'default_credit':
                 self.sen_att_name = 'sex'
                 self.target_name = 'default_payment_next_month'
+                self.priv = 0
             _, processed_data = load_dataset(self.dataset_name)
             if self.dataset_name == 'census_income_kdd':
                 processed_data = processed_data.sample(frac=0.1, random_state=25) 
@@ -110,8 +119,7 @@ class BenchMarkPreprocessingMethods:
 
                 # Mitigate bias
                 if self.sota_method == 'disparate_impact':
-                    X_train_repair = self._disparate_impact_remover(repair_level=1.0, sensitive_attribute=self.sen_att_name, X=X_train)
-                    X_val_repair = self._disparate_impact_remover(repair_level=1.0, sensitive_attribute=self.sen_att_name, X=X_val)
+                    X_train_repair, X_val_repair = self._disparate_impact_remover(repair_level=1.0, sensitive_attribute=self.sen_att_name, X_train=X_train, X_val=X_val)
                     model = XGBClassifier()
                     model.fit(X_train_repair, y_train)
                     
@@ -134,8 +142,18 @@ class BenchMarkPreprocessingMethods:
                     processed_eo.append(eo)
                     processed_pqp.append(pqp)
                     
-                elif self.sota_method == 'reweight':
-                    self.reweight(X_train, y_train, X_val, y_val)
+                elif self.sota_method == 'reweighing':
+                    model = XGBClassifier()
+                    instance_weights = self._reweighing(X_train, y_train)
+                    model.fit(X_train, y_train, sample_weight=instance_weights)
+                    accuracy, dr, dp, eo, pqp = self._run_evaluation_pd(model, X_val, y_val)
+                    processed_accuracy.append(accuracy)
+                    processed_dr.append(dr)
+                    processed_dp.append(dp)
+                    processed_eo.append(eo)
+                    processed_pqp.append(pqp)
+                    
+
                 elif self.sota_method == 'fairUS':
                     self.fairUS(X_train, y_train, X_val, y_val)
                 elif self.sota_method == 'optimized_preprocessing':
@@ -189,19 +207,23 @@ class BenchMarkPreprocessingMethods:
 
 
 
-    def _disparate_impact_remover(self, repair_level, sensitive_attribute, X):
+    def _disparate_impact_remover(self, repair_level, sensitive_attribute, X_train:pd.DataFrame, X_val:pd.DataFrame):
         DisparateImpactRemover(repair_level=1.0, sensitive_attribute=self.sen_att_name)
         from BlackBoxAuditing.repairers.GeneralRepairer import Repairer
         self.Repairer = Repairer
-        features = X.values.tolist()
-        index = X.columns.tolist().index(self.sen_att_name)
-        repairer = self.Repairer(features, index, repair_level, False)
+        features_train = X_train.values.tolist()
+        features_val = X_val.values.tolist()
+        index = X_train.columns.tolist().index(self.sen_att_name)
+        repairer = self.Repairer(features_train, index, repair_level, False)
 
-        repaired_features = repairer.repair(features)
-        repaired_features = np.array(repaired_features, dtype=np.float64)
+        repaired_features_train = repairer.repair(features_train)
+        repaired_features_val = repairer.repair(features_val)
+        repaired_features_train = np.array(repaired_features_train, dtype=np.float64)
+        repaired_features_val = np.array(repaired_features_val, dtype=np.float64)
 
-        repaired_features[:, index] = X.values[:, index]
-        return repaired_features
+        repaired_features_train[:, index] = X_train.values[:, index]
+        repaired_features_val[:, index] = X_val.values[:, index]
+        return repaired_features_train, repaired_features_val
     
     def _correlation_removal(self, X:pd.DataFrame, remove_ratio=1):
         cr = CorrelationRemover(sensitive_feature_ids=[self.sen_att_name], alpha=remove_ratio)
@@ -209,14 +231,56 @@ class BenchMarkPreprocessingMethods:
         # CorrelationRemover(sensitive_feature_ids=sensitive_feature)
         X_transform_without_sen_att = cr.transform(X)
         sensitive_column = X['sex'].copy()
-        X_transform_with_sensitive = np.insert(X_transform_without_sen_att, 0, sensitive_column, axis=1)
+        X_transform_with_sensitive = np.insert(X_transform_without_sen_att, self.sen_att_index, sensitive_column, axis=1)
         return X_transform_with_sensitive
+
+    def _reweighing(self, X_train, y_train):
+        # self.w_p_fav = 1.
+        # self.w_p_unfav = 1.
+        # self.w_up_fav = 1.
+        # self.w_up_unfav = 1.
+        df = X_train.copy()
+        df[self.target_name] = y_train
+
+        priv_cond = X_train[self.sen_att_name] == self.priv
+        unpriv_cond = X_train[self.sen_att_name] != self.priv
+        fav_cond = y_train == 1
+        unfav_cond = y_train == 0
+
+        n = len(X_train)
+        n_p = np.sum(priv_cond)
+        n_up = np.sum(unpriv_cond)
+        n_fav = np.sum(fav_cond)
+        n_unfav = np.sum(unfav_cond)
+        
+        # 计算 (group, label) 组合的样本数
+        n_p_fav = np.sum(priv_cond & fav_cond)
+        n_p_unfav = np.sum(priv_cond & unfav_cond)
+        n_up_fav = np.sum(unpriv_cond & fav_cond)
+        n_up_unfav = np.sum(unpriv_cond & unfav_cond)
+
+        # 计算 reweighing 权重
+        w_p_fav = (n_fav * n_p) / (n * n_p_fav) if n_p_fav > 0 else 1.
+        w_p_unfav = (n_unfav * n_p) / (n * n_p_unfav) if n_p_unfav > 0 else 1.
+        w_up_fav = (n_fav * n_up) / (n * n_up_fav) if n_up_fav > 0 else 1.
+        w_up_unfav = (n_unfav * n_up) / (n * n_up_unfav) if n_up_unfav > 0 else 1.
+
+        instance_weights = np.ones(len(df))
+        instance_weights[priv_cond & fav_cond] *= w_p_fav
+        instance_weights[priv_cond & unfav_cond] *= w_p_unfav
+        instance_weights[unpriv_cond & fav_cond] *= w_up_fav
+        instance_weights[unpriv_cond & unfav_cond] *= w_up_unfav
+
+        return instance_weights
+
+
+
 
 
     def _run_evaluation_pd(self, model, X_val:pd.DataFrame, y_val:pd.Series):
         sen_att_name = [self.sen_att_name]
         sen_att = self.sen_att
-        priv_val = [1]
+        priv_val = [self.priv]
         unpriv_dict = [list(set(X_val.values[:, sa])) for sa in sen_att]
         for sa_list, pv in zip(unpriv_dict, priv_val):
             sa_list.remove(pv)
